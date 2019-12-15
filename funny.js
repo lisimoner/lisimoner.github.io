d6c26bbb36c198e36302e769db06edb684383ed0
simon <15927521530@163.com>
2019-12-15 17:50:33 +0800

    Site updated: 2019-12-15 17:50:27

---
+++ funny.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヽ(●-`Д´-)ノ美女别走啊';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
+         document.title = 'ヾ(Ő∀Ő3)ノ美女你来了';
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 1000);
+     }
+ });
